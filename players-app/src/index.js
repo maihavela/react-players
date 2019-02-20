e46699d0a21a2d90d99079e181dbb830f9cec222
maihavela <mailen.havela@gmail.com>
2019-02-20 00:24:30 -0300

    Add mounted  and connecr to playersSelector

--- players-app/src/index.js
+++ players-app/src/index.js
@@ -1,43 +1,60 @@
-import React from "react";
+import React, { Component } from 'react';
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
+import { Provider, connect } from "react-redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 import './index.css';
-import Title from './components/Title/Title.js';
-import SearchBar from './components/SearchBar/SearchBar.js';
-import PlayersList from './components/PlayersList/PlayersList.js';
-import Container from 'react-bootstrap/Container';
+import { PlayersList, SearchBar, Title } from './components/index';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.css';
-//import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './serviceWorker';
+import { arrayOf, object } from 'prop-types';
+import { fetchPlayers } from './actions/playersActions';
+import { playersSelector } from './selectors/PlayersSelector';
 
 const store = createStore(
   rootReducer,
   applyMiddleware(thunk)
 );
 
-function App() {
-  return (
-    <div className="App">
-      <Container>
-        <Row>
-          <Col xs={4} md={8} lg={12}>
-            <Title title={'Football Player Finder'} />
-          </Col>
-          <Col xs={4} md={8} lg={12}>
-            <SearchBar />
-          </Col>
-          <Col xs={4} md={8} lg={12}>
-            <PlayersList />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchPlayers();
+  }
+
+  render() {
+    const { players } = this.props;
+
+    return (
+      <div className="App">
+        <Container>
+          <Row>
+            <Col xs={4} md={8} lg={12}>
+              <Title title={'Football Player Finder'} />
+            </Col>
+            <Col xs={4} md={8} lg={12}>
+              <SearchBar />
+            </Col>
+            <Col xs={4} md={8} lg={12}>
+              <PlayersList players={players} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
+
+App.propTypes = {
+  players: arrayOf(object)
+};
+
+const mapToStateProps = state => ({ players: playersSelector(state)});
+
+export default connect(mapToStateProps, fetchPlayers);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
@@ -46,3 +63,4 @@ ReactDOM.render(
   </Provider>,
   rootElement
 );
+registerServiceWorker();
