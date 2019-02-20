9f346eacf0dae5f5869cee06be4f35fe271c1015
maihavela <mailen.havela@gmail.com>
2019-02-20 00:19:33 -0300

    Add index classes to export/import

---
+++ players-app/src/components/index.js
@@ -0,0 +1,6 @@
+
+import { PlayersList } from './PlayersList/index';
+import { SearchBar } from './SearchBar/index';
+import { Title } from './Title/index';
+
+export { PlayersList, SearchBar, Title };
